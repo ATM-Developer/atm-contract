256785739026e2e75298b2625606546e149dee16
zhangming <zm20258888@outlook.com>
2021-08-16 11:42:54 +0800

    platy contracts

--- contracts/token/WLUCA/IWluca.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-import "./ERC20.sol";
-interface Iwluca {
-    function mint(address user, uint amount) external;
-    function burn(uint amount) external;
-}
